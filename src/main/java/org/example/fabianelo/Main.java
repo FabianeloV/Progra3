0e840970d944228f530b26d497f0e29e0c9904ad
FabianeloV <fevr.developer@gmail.com>
2025-03-31 10:45:13 -0500

    Clase 31 marzo

--- src/main/java/org/example/fabianelo/Main.java
+++ src/main/java/org/example/fabianelo/Main.java
@@ -1,7 +1,17 @@
 package org.example.fabianelo;
 
+import org.example.fabianelo.marzo31.Persona;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Persona persona = new Persona("Pablito", 'M', new Date(2004,05,24), "Estudiante");
+
+        System.out.println("Nombre de la persona: "+ persona.nombre);
+        System.out.println("Sexo: "+ persona.getSexo());
+
+        persona.setNombre("Fabianelo");
+        System.out.println("Nuevo nombre de la persona: "+ persona.nombre);
     }
 }
