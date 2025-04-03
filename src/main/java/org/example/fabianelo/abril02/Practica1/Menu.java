39d5c75dff09ce354f900f6f581c660ef0922799
FabianeloV <fevr.developer@gmail.com>
2025-04-02 21:38:11 -0500

    Clase 2 abril - Arreglos nocturnos

--- src/main/java/org/example/fabianelo/abril02/Practica1/Menu.java
+++ src/main/java/org/example/fabianelo/abril02/Practica1/Menu.java
@@ -2,8 +2,52 @@ package org.example.fabianelo.abril02.Practica1;
 
 import java.util.Scanner;
 
-public interface Menu {
-    static Scanner scanner = new Scanner(System.in);
+public class Menu {
+    public Menu() {}
 
-    public void crearComputador(Computador computador);
+    public void mostrarOpciones() {
+        System.out.print("------------------------------------\n");
+        System.out.println("Escoja una opción: ");
+        System.out.println("1. Listar datos del computador");
+        System.out.println("2. Agregar computador");
+        System.out.println("3. Eliminar computador");
+        System.out.println("4. Salir");
+        System.out.print("------------------------------------\n");
+    }
+
+    public void crearComputador(Computador computador) {
+        //Declarar el scanner
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre del computador: ");
+        computador.setNombre(sc.nextLine());
+
+        System.out.println("Ingrese el modelo del computador: ");
+        computador.setModelo(sc.nextLine());
+
+        System.out.println("Ingrese el precio del computador: ");
+        computador.setPrecio(sc.nextInt());
+
+        System.out.println("Ingrese la ram del computador: ");
+        computador.setRam(sc.nextInt());
+    }
+
+    public void listarComputador(Computador computador) {
+        if (computador.getModelo() != null) {
+            System.out.println("Nombre del computador: " + computador.getNombre());
+            System.out.println("Modelo del computador: " + computador.getModelo());
+            System.out.println("Precio del computador: " + computador.getPrecio() + "$");
+            System.out.println("Ram del computador: " + computador.getRam() + "Gb\n");
+        } else {
+            System.out.println("El computador no existe\n");
+        }
+    }
+
+    public void eliminarComputador(Computador computador) {
+        computador.setNombre(null);
+        computador.setModelo(null);
+        computador.setPrecio(null);
+        computador.setRam(null);
+        System.out.println("Computador eliminado correctamente\n");
+    }
 }
--- src/main/java/org/example/fabianelo/abril02/Practica1/Practica1.java
+++ src/main/java/org/example/fabianelo/abril02/Practica1/Practica1.java
@@ -4,26 +4,35 @@ import java.util.Scanner;
 
 public class Practica1 {
     public static void main(String[] args) {
-        //Declarar el scanner
+
         Scanner sc = new Scanner(System.in);
+
+        Menu menu = new Menu();
 
         Computador computador = new Computador();
 
-        System.out.println("Ingrese el nombre del computador: ");
-        computador.setNombre(sc.nextLine());
+        boolean option = true;
 
-        System.out.println("Ingrese el modelo del computador: ");
-        computador.setModelo(sc.nextLine());
-
-        System.out.println("Ingrese el precio del computador: ");
-        computador.setPrecio(sc.nextInt());
-
-        System.out.println("Ingrese la ram del computador: ");
-        computador.setRam(sc.nextInt());
-
-        System.out.println("Nombre del computador: " + computador.getNombre());
-        System.out.println("Modelo del computador: " + computador.getModelo());
-        System.out.println("Precio del computador: " + computador.getPrecio());
-        System.out.println("Ram del computador: " + computador.getRam());
+        while (option) {
+            menu.mostrarOpciones();
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    menu.listarComputador(computador);
+                    break;
+                case 2:
+                    menu.crearComputador(computador);
+                    break;
+                case 3:
+                    menu.eliminarComputador(computador);
+                    break;
+                case 4:
+                    option = false;
+                    break;
+                default:
+                    System.out.println("Opción no valida\n");
+                    break;
+            }
+        }
     }
 }
