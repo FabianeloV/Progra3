44c4ae8ef5ad1d75f0e5cbf30542b9c544a1dba7
FabianeloV <fevr.developer@gmail.com>
2025-06-03 19:06:42 -0500

    Tests

--- src/test/java/org/example/fabianelo/Practica1MVC/controlador/CalcularMasaTest.java
+++ src/test/java/org/example/fabianelo/Practica1MVC/controlador/CalcularMasaTest.java
@@ -6,8 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalcularMasaTest extends CalcularMasa {
 
+    // Test acertado
     @Test
     void testCalcularMasa() {
         assertEquals(24.618105F, calcularMasa(78, 1.78F));
     }
+
+    // Test erróneo
+    @Test
+    void testCalcularMasa1() {
+        assertEquals(27, calcularMasa(76, 1.75F));
+    }
+
+    // Test acertado
+    @Test
+    void testCalcularMasa2() {
+        assertEquals(24.816326F, calcularMasa(76, 1.75F));
+    }
+
+    //Test para validar que no pasen entradas vacias
+    @Test
+    void testValidarEntradas() {
+        assertEquals("No puede estar vacio ningún campo", validarEntradas("",""));
+    }
+
 }
